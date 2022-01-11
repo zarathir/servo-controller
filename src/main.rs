4a5b4c88895612c81d29df19f550793898c40670
Michael Tal <michael.tal@protonmail.ch>
2022-01-11 08:18:57 +0100

    added get_input function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use servo_control::{ServoController, ServoMotor};
-use std::{io::stdin, thread, time};
+use std::{io::stdin, num::ParseIntError, thread, time};
 
 fn sleep(time: u64) {
     thread::sleep(time::Duration::from_millis(time * 1000));
+}
+
+fn get_input() -> Result<u32, ParseIntError> {
+    let mut input = String::new();
+    stdin()
+        .read_line(&mut input)
+        .expect("Did not enter a correct string");
+
+    match input.trim().parse::<u32>() {
+        Ok(angle) => Ok(angle),
+        Err(err) => Err(err),
+    }
 }
 
 fn main() {
@@ -13,15 +25,10 @@ fn main() {
     let start_angle = motor_1.get_angle();
     controller.set_motor_angle(&mut motor_1, start_angle);
 
-    let mut input = String::new();
-
     loop {
         println!("Where do you want to move the servo?");
-        stdin()
-            .read_line(&mut input)
-            .expect("Did not enter a correct string");
 
-        match input.trim().parse::<u32>() {
+        match get_input() {
             Ok(angle) => {
                 controller.set_motor_angle(&mut motor_1, angle);
                 println!("Moving servo to {}", angle);
